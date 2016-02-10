65a345f4aafa91643a246cf8a9b837e23d067350
jciancio <jciancio7@gmail.com>
2016-02-10 11:51:55 -0500

    generated urls controller

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,7 @@
+class UrlsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'urls/index'
+
+  get 'urls/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
