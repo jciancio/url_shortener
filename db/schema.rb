97e24608478dd495a7d5b58f465b253477517d54
jciancio <jciancio7@gmail.com>
2016-02-11 11:58:37 -0500

    added rails_12factor

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160210162835) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "urls", force: :cascade do |t|
     t.string   "original"
     t.string   "short"
